a645181ce881460947424daccaad20db7f166cf7
Leonardo Merlo Primieri <leo.primieri@gmail.com>
2020-07-01 18:53:40 -0300

    done!

--- jogo.py
+++ jogo.py
@@ -5,6 +5,11 @@ from functions.userFunctions import userInfos
 
 userInfos()
 
+log = open('userInfo.txt', 'a')
+name = input('Informe seu nome: ')
+email = input('Informe seu email: ')
+log.write(f' \nNome do jogador: {name} | E-mail do jogador: {email}')
+log.close()
 
 pygame.init()
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ executables = [cx_Freeze.Executable(
     script="jogo.py", icon="assets/icon.ico")]
 cx_Freeze.setup(
     name="Mandalorian Adventure",
-    options={"build_exe": {"packages": ["pygame"], "include_files": ["assets"]
+    options={"build_exe": {"packages": ["pygame"], "include_files": ["assets", "functions"]
                            }},
     executables=executables
 )
